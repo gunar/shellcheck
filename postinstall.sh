0ceaa3fe2c3a25e0926a1dbb1d46c48a4e645b38
Gunar C. Gessner <gunar@gunargessner.com>
2017-11-28 12:40:40 +0100

    lets try again without placeholder

--- postinstall.sh
+++ postinstall.sh
@@ -4,9 +4,5 @@
 wget https://storage.googleapis.com/shellcheck/shellcheck-latest.linux.x86_64.tar.xz
 tar xf shellcheck-latest.linux.x86_64.tar.xz
 
-# Replace placeholder
-mv shellcheck-latest/shellcheck .
-
 # Clean up
-rm -rf shellcheck-latest
 rm shellcheck-latest.linux.x86_64.tar.xz
