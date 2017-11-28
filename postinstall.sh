d8e66186e895fe404c4108e1cfcf2a5739076e9b
Gunar C. Gessner <gunar@gunargessner.com>
2017-11-28 12:12:40 +0100

    placeholder

--- postinstall.sh
+++ postinstall.sh
@@ -1,11 +1,12 @@
-#!/bin/sh
+#!/bin/sh -e
 
 # Download
 wget https://storage.googleapis.com/shellcheck/shellcheck-latest.linux.x86_64.tar.xz
-tar xf shellcheck-latest.linux.x86_64.tar.xz
+# tar xf shellcheck-latest.linux.x86_64.tar.xz
 
 # "Install"
-ln -fs "$(readlink -f ./shellcheck-latest/shellcheck)" $(npm bin)/shellcheck
+# mkdir -p $(npm bin)
+# ln -fs "$(readlink -f ./shellcheck-latest/shellcheck)" $(npm bin)/shellcheck
 
 # Clean up
 rm shellcheck-latest.linux.x86_64.tar.xz
