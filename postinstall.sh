646f14a04111cfc5e69ac7d32f8bea14482ae29e
Jason Karns <jason@karns.name>
2018-11-02 12:10:07 -0400

    Fetch with curl or wget

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,28 @@
 #!/bin/sh -e
 
+http_client() {
+  for client in curl wget; do
+    if type "$client" >/dev/null 2>/dev/null; then
+      echo "$client"
+      return
+    fi
+  done
+  echo "error: please install \`curl\` or \`wget\` and try again" >&2
+  return 1
+}
+
+curl_get() {
+  curl --disable --silent --show-error --fail --location --remote-name "$1"
+}
+
+wget_get() {
+  wget --no-config --no-verbose "$1"
+}
+
 # Download
-wget https://storage.googleapis.com/shellcheck/shellcheck-latest.linux.x86_64.tar.xz
+"$(http_client)_get" https://storage.googleapis.com/shellcheck/shellcheck-latest.linux.x86_64.tar.xz
+
+# Extract
 tar xf shellcheck-latest.linux.x86_64.tar.xz
 
 # Clean up
