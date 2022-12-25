2a5e8e3b57f99a64c54eea702e1fc2fbc1580b14
carlocorradini <carlo.ita98@gmail.com>
2022-12-25 15:48:11 +0100

    test: added platform and architecture tests

--- tests/shellcheck/architecture.test.ts
+++ tests/shellcheck/architecture.test.ts
@@ -1,163 +1,50 @@
 import { shellCheckArchitecture } from '~/utils';
 import { ArchitectureError } from '~/errors';
-import { arrayOfAll } from '../utils';
-
-const ARCHITECTURES: Array<NodeJS.Architecture> =
-  arrayOfAll<NodeJS.Architecture>()([
-    'arm',
-    'arm64',
-    'ia32',
-    'mips',
-    'mipsel',
-    'ppc',
-    'ppc64',
-    's390',
-    's390x',
-    'x64'
-  ]);
-
-const DARWIN_ARCHITECTURES: Array<NodeJS.Architecture> = ['x64'];
-
-const LINUX_ARCHITECTURES: Array<NodeJS.Architecture> = ['x64', 'arm64'];
-
-const WIN32_ARCHITECTURES: Array<NodeJS.Architecture> = ['x64'];
+import {
+  ARCHITECTURES_SUPPORTED,
+  ARCHITECTURES_NOT_SUPPORTED
+} from '../helpers';
 
 describe('Architecture', () => {
-  test.each(DARWIN_ARCHITECTURES)(
-    `should succeed supporting 'darwin' platform '%s' architecture`,
-    (architecture) => {
+  it.each(
+    Object.entries(ARCHITECTURES_SUPPORTED)
+      .filter(([, architectures]) => architectures.length > 0)
+      .reduce<Array<[NodeJS.Platform, NodeJS.Architecture]>>(
+        (accumulator, current) => {
+          current[1].forEach((architecture) => {
+            accumulator.push([current[0] as NodeJS.Platform, architecture]);
+          });
+          return accumulator;
+        },
+        []
+      )
+  )(
+    `should succeed supporting platform '%s' architecture '%s'`,
+    (platform, architecture) => {
       expect(() =>
-        shellCheckArchitecture({ platform: 'darwin', architecture })
+        shellCheckArchitecture({ platform, architecture })
       ).not.toThrow();
     }
   );
 
-  test.each(
-    ARCHITECTURES.filter(
-      (architecture) => !DARWIN_ARCHITECTURES.includes(architecture)
-    )
+  it.each(
+    Object.entries(ARCHITECTURES_NOT_SUPPORTED)
+      .filter(([, architectures]) => architectures.length > 0)
+      .reduce<Array<[NodeJS.Platform, NodeJS.Architecture]>>(
+        (accumulator, current) => {
+          current[1].forEach((architecture) => {
+            accumulator.push([current[0] as NodeJS.Platform, architecture]);
+          });
+          return accumulator;
+        },
+        []
+      )
   )(
-    `should fail supporting 'darwin' platform '%s' architecture`,
-    (architecture) => {
-      expect(() =>
-        shellCheckArchitecture({ platform: 'darwin', architecture })
-      ).toThrow(ArchitectureError);
-    }
-  );
-
-  test.each(LINUX_ARCHITECTURES)(
-    `should succeed supporting 'linux' platform '%s' architecture`,
-    (architecture) => {
-      expect(() =>
-        shellCheckArchitecture({ platform: 'linux', architecture })
-      ).not.toThrow();
-    }
-  );
-
-  test.each(
-    ARCHITECTURES.filter(
-      (architecture) => !LINUX_ARCHITECTURES.includes(architecture)
-    )
-  )(
-    `should fail supporting 'linux' platform '%s' architecture`,
-    (architecture) => {
-      expect(() =>
-        shellCheckArchitecture({ platform: 'linux', architecture })
-      ).toThrow(ArchitectureError);
-    }
-  );
-
-  test.each(WIN32_ARCHITECTURES)(
-    `should succeed supporting 'win32' platform '%s' architecture`,
-    (architecture) => {
-      expect(() =>
-        shellCheckArchitecture({ platform: 'win32', architecture })
-      ).not.toThrow();
-    }
-  );
-
-  test.each(
-    ARCHITECTURES.filter(
-      (architecture) => !WIN32_ARCHITECTURES.includes(architecture)
-    )
-  )(
-    `should fail supporting 'win32' platform '%s' architecture`,
-    (architecture) => {
-      expect(() =>
-        shellCheckArchitecture({ platform: 'win32', architecture })
-      ).toThrow(ArchitectureError);
-    }
-  );
-
-  test.each(ARCHITECTURES)(
-    `should fail supporting 'aix' platform '%s' architecture`,
-    (architecture) => {
-      expect(() =>
-        shellCheckArchitecture({ platform: 'aix', architecture })
-      ).toThrow(ArchitectureError);
-    }
-  );
-
-  test.each(ARCHITECTURES)(
-    `should fail supporting 'android' platform '%s' architecture`,
-    (architecture) => {
-      expect(() =>
-        shellCheckArchitecture({ platform: 'android', architecture })
-      ).toThrow(ArchitectureError);
-    }
-  );
-
-  test.each(ARCHITECTURES)(
-    `should fail supporting 'cygwin' platform '%s' architecture`,
-    (architecture) => {
-      expect(() =>
-        shellCheckArchitecture({ platform: 'cygwin', architecture })
-      ).toThrow(ArchitectureError);
-    }
-  );
-
-  test.each(ARCHITECTURES)(
-    `should fail supporting 'freebsd' platform '%s' architecture`,
-    (architecture) => {
-      expect(() =>
-        shellCheckArchitecture({ platform: 'freebsd', architecture })
-      ).toThrow(ArchitectureError);
-    }
-  );
-
-  test.each(ARCHITECTURES)(
-    `should fail supporting 'haiku' platform '%s' architecture`,
-    (architecture) => {
-      expect(() =>
-        shellCheckArchitecture({ platform: 'haiku', architecture })
-      ).toThrow(ArchitectureError);
-    }
-  );
-
-  test.each(ARCHITECTURES)(
-    `should fail supporting 'netbsd' platform '%s' architecture`,
-    (architecture) => {
-      expect(() =>
-        shellCheckArchitecture({ platform: 'netbsd', architecture })
-      ).toThrow(ArchitectureError);
-    }
-  );
-
-  test.each(ARCHITECTURES)(
-    `should fail supporting 'openbsd' platform '%s' architecture`,
-    (architecture) => {
-      expect(() =>
-        shellCheckArchitecture({ platform: 'openbsd', architecture })
-      ).toThrow(ArchitectureError);
-    }
-  );
-
-  test.each(ARCHITECTURES)(
-    `should fail supporting 'sunos' platform '%s' architecture`,
-    (architecture) => {
-      expect(() =>
-        shellCheckArchitecture({ platform: 'sunos', architecture })
-      ).toThrow(ArchitectureError);
+    `should fail supporting platform '%s' architecture '%s'`,
+    (platform, architecture) => {
+      expect(() => shellCheckArchitecture({ platform, architecture })).toThrow(
+        ArchitectureError
+      );
     }
   );
 });
